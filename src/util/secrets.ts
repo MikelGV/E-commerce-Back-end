84f5a9e6a7bb5a2987d8c01913655666fd260170
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-24 13:05:05 +0200

    logger

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -1,9 +1,9 @@
-//import logger from './logger';
+import logger from './logger';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
 if (fs.existsSync(".env")) {
-//    logger.debug("Using .env file to supply config environment variables");
+    logger.debug("Using .env file to supply config environment variables");
     dotenv.config({ path: ".env" });
 }
 
@@ -14,15 +14,15 @@ export const SESSION_SECRET = process.env["SESSION_SECRET"];
 export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
 
 if (!SESSION_SECRET) {
-//    logger.error("No client secret. Set SESSION_SECRET environment varible.");
+    logger.error("No client secret. Set SESSION_SECRET environment varible.");
     process.exit(1);
 }
 
 if (!MONGODB_URI) {
     if (prod) {
-//        logger.error("No mongo connection string. Set MONGODB_URI environment variable");
+       logger.error("No mongo connection string. Set MONGODB_URI environment variable");
     } else {
-//        logger.error("No mongo connection string. Set MONGODB_URI_LOCAL environment variable");
+        logger.error("No mongo connection string. Set MONGODB_URI_LOCAL environment variable");
     }
     process.exit(1);
 }
