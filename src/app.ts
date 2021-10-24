431d932a1c1c2a2beeb076ac6925df8f5dfc2aaa
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-24 13:24:38 +0200

    added session

--- src/app.ts
+++ src/app.ts
@@ -3,24 +3,29 @@ import session from "express-session";
 import mongoose from "mongoose";
 import path from "path";
 import passport from "passport";
+import MongoStore from "connect-mongo";
 
 import {MONGODB_URI, SESSION_SECRET} from "./util/secrets";
-import { MONGODB_PASSWORD } from "./noEnv";
+import { MONGODB_PASSWORD, SESSION_SECRETS } from "./noEnv";
 
 import * as passportConfig from "./config/passport";
 
 
 const app = express();
+const monogoUrl = MONGODB_PASSWORD
 
-mongoose.connect(MONGODB_PASSWORD).then(() => {
+mongoose.connect(monogoUrl).then(() => {
     console.log("Connected to mongodb!")
 }).catch(err => {
     console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`)
 });
 
 app.set("port", process.env.PORT || 4000);
-app.use(passport.initialize());
-//app.use(passport.session());
+app.use(session({
+    resave: true,
+    saveUninitialized:true,
+    secret: SESSION_SECRETS,
+}))
 
 app.use(passport.initialize());
 app.use(passport.session());
