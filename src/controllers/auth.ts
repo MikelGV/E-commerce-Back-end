556b3e8f5782f13dfd115352aec091f3d904fde4
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-25 09:29:45 +0200

    signup not working

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -12,30 +12,39 @@ import { Callback, NativeError } from "mongoose";
  * GET /login
  */
 
-export const signup = async (req: Request, res: Response, next: NextFunction) => {
-    // const errors = validationResult(req);
-    // if (!errors) {
-    //     const error = new Error("Validation failed.");
-    //     error.statusCode = 422;
-    //     error.data = errors.array();
-    //     throw error
-    // }
-    const email = req.body.email;
-    const username = req.body.username;
-    const password = req.body.password
+export const signup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    await check("email", "Email is not valid").isEmail().run(req);
+    await check("username", "Username is not valid").run(req);
+    await check("password", "Password must be at least 4 characters long").isLength({ min:4 }).run(req);
+    await check("confirmPassword", "Passwords do not match").equals(req.body.password).run(req);
+    await body("email").normalizeEmail({ gmail_remove_dots: false }).run(req);
 
-    try {
-        const user = new User({
-            email: email,
-            username: username,
-            password: password
-        });
-        const result = await user.save();
-        res.status(201).json({ message: "User created!", userId: result._id })
-    } catch (err) {
-        // if (!err.statuscCode) {
-        //     err.statuscCode = 500;
-        // }
-        // next(err)
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        //req.flash("errors", errors.array());
+        return res.redirect("/signup");
     }
+
+    const user = new User({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.findOne({ email: req.body.email }, (err: NativeError, existingUser: UserDocument) => {
+        if (err) {return next(err)}
+        if (existingUser) {
+            return res.redirect("/signup");
+        }
+        user.save((err) => {
+            if (err) { return next(err); }
+            req.logIn(user, (err) => {
+                if (err) {
+                    return next(err);
+                }
+                res.status(201).json({ message: "User created!", userId: user._id })
+            });
+        });
+    })
 };
