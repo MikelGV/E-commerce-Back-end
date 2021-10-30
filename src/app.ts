72aeba761cfb2c9eaa66d5062733a2bbf07d74e9
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-30 10:10:50 +0200

    status message and data added

--- src/app.ts
+++ src/app.ts
@@ -47,6 +47,14 @@ app.use((req, res, next) => {
         next();
 });
 
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+  });
+
 
 app.post("/signup", authController.signup)
 
