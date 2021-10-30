e92fe6bb7963b15074ae2b8b97b1b7ee690bcb2a
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-30 10:11:05 +0200

    trying things

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -18,39 +18,29 @@ import { Callback, NativeError } from "mongoose";
  * POST /signup
  */
 export const signup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    await check("email", "Email is not valid").isEmail().run(req);
-    await check("username", "Username is not valid").run(req);
-    await check("password", "Password must be at least 4 characters long").isLength({ min:4 }).run(req);
-    // await check("confirmPassword", "Passwords do not match").equals(req.body.password).run(req); // i don't know why this fails
-    await body("email").normalizeEmail({ gmail_remove_dots: false }).run(req);
-
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
-        //req.flash("errors", errors.array());
-        // return res.redirect("/signup");
+        const error = new Error('Validation failed.');
+        error.statusCode = 422;
+        error.data = errors.array();
+        throw error;
     }
-
-    const user = new User({
-        email: req.body.email,// i don't know why this fails
-        username: req.body.username,
-        password: req.body.password 
-    });
-    console.log(user)
-
-    User.findOne({ email: req.body.email }, (err: NativeError, existingUser: UserDocument) => {
-        if (err) {return next(err)}
-        if (existingUser) {
-            return res.status(500).json({ message: "User already exists", userId: user._id })
-        }
-        user.save((err) => {
-            if (err) { return next(err); }
-            req.logIn(user, (err) => {
-                if (err) {
-                    return next(err);
-                }
-                res.status(201).json({ message: "User created!", userId: user._id })
-            });
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    try {
+        const user = new User({
+        email: email,
+        password: password,
+        name: name
         });
-    })
+        const result = await user.save();
+        res.status(201).json({ message: 'User created!', userId: result._id });
+    } catch (err) {
+        if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
+        
+    }
 };
