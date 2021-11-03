69cb2b656105667099f600b60966c3e5b34081c8
Mikel Galdos <mikelgaldosv@gmail.com>
2021-11-03 03:07:13 +0100

    signup endpoint added

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,10 @@
-import crypto from "crypto";
-import passport from "passport";
+// External imports
 import { User, UserDocument, AuthToken } from "../models/user";
 import { Request, Response, NextFunction } from "express";
 import { body, check, validationResult } from "express-validator";
-import "../config/passport";
 import { Callback, NativeError } from "mongoose";
-import { IVerifyOptions } from "passport-local";
+// my imports
+import "../config/passport";
 
 
 /**
@@ -13,40 +12,33 @@ import { IVerifyOptions } from "passport-local";
  * POST /login
  */
 export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    await check("email", "Email is not valid").isEmail().run(req);
-    await check("password", "Password cannot be blank").isLength({min: 1}).run(req);
-    await body("email").normalizeEmail({gmail_remove_dots: false}).run(req);
 
-    passport.authenticate("local", (err: Error, user:UserDocument, info:IVerifyOptions) => {
-        if (err) {
-            return next(err)
-        }
-        if (!user) {
-            
-        }
-    })
+    const errors = validationResult(req);
+
 }
 
 /**
  *  Signup
  * POST /signup
  */
-export const signup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // const errors = validationResult(req);
+export const signup = async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
     // if (!errors.isEmpty()) {
     //     const error = new Error('Validation failed.');
     //     error.statusCode = 422;
     //     error.data = errors.array();
     //     throw error;
-    // }
+    //   }
+
     const email = req.body.email;
-    const name = req.body.name;
+    const username = req.body.username;
     const password = req.body.password;
     try {
         const user = new User({
             email: email,
             password: password,
-            name: name
+            username: username
         });
         const result = await user.save();
         res.status(201).json({ message: 'User created!', userId: result._id });
