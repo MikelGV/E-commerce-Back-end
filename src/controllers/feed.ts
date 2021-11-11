d479101cb7a7a894dd8fb0e102de81b1254402c9
Mikel Galdos <mikelgaldosv@gmail.com>
2021-11-11 09:02:43 +0100

    addToCart added

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -1,6 +1,10 @@
 // Imports
 import { Product } from "../models/product";
 import path from "path";
+import { NextFunction, Response, Request } from "express";
 
 
 const ITEMS_PER_PAGE = 2;
+
+
+export const addToCart = async (req: Request, res: Response, next: NextFunction): Promise<void> => {}
