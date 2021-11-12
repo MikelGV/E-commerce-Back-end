2cf59d7eab5b42f479b2f71655d940b2c20937b7
Mikel Galdos <mikelgaldosv@gmail.com>
2021-11-12 05:13:34 +0100

    first things of addProduct

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -10,7 +10,12 @@ const ITEMS_PER_PAGE = 2;
 /**
  * Create Product
  */
-export const addProduct = async (req: Request, res: Response, next: NextFunction) => {}
+export const addProduct = async (req: Request, res: Response, next: NextFunction) => {
+    const title = req.body.title;
+    const price = req.body.price;
+    const description = req.body.description;
+    //const imageUrl = req.body.imageUrl;
+}
 
 /**
  * Add to cart
