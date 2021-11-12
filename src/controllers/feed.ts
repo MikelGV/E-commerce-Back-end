495a741c5c1971dfa1d60988b4cc51d22db60cfa
Mikel Galdos <mikelgaldosv@gmail.com>
2021-11-12 05:07:22 +0100

    addProduct added

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -7,4 +7,12 @@ import { NextFunction, Response, Request } from "express";
 const ITEMS_PER_PAGE = 2;
 
 
+/**
+ * Create Product
+ */
+export const addProduct = async (req: Request, res: Response, next: NextFunction) => {}
+
+/**
+ * Add to cart
+ */
 export const addToCart = async (req: Request, res: Response, next: NextFunction): Promise<void> => {}
