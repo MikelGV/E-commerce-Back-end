3a92ce55043537db78dd891dfcd083e0d5d5261a
MikelGV <mikelgaldosv@gmail.com>
2021-11-24 16:39:49 +0100

    trying to solve errors

--- src/middleware/is-auth.ts
+++ src/middleware/is-auth.ts
@@ -2,24 +2,24 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 export = (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.get('Authorization');
+    const authHeader = req.get['Authorization'];
     if (!authHeader) {
         const error = new Error("Not authenticated");
-        res.sendStatus(401);
-        throw error
+        res.status(401);
+        next(error)
     }
-    const token = authHeader.split(' ')[1];
+    const token = authHeader?.split(' ')[1];
     let decodedToken;
     try {
         decodedToken = jwt.verify(token, 'supersecret');
     } catch (err: any) {
         err.statuCode = 500;
-        throw err;
+        next(err);
     }
     if (!decodedToken) {
         const error = new Error('Not authenticated');
-        res.sendStatus(401);
-        throw error
+        res.status(401);
+        next(error)
     }
     req.userId = decodedToken.userId;
     next();
