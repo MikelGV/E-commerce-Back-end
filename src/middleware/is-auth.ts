c2e95eae712dba969671f0dc40b5b9cb201a5900
MikelGV <mikelgaldosv@gmail.com>
2021-11-24 16:06:10 +0100

    test

--- src/middleware/is-auth.ts
+++ src/middleware/is-auth.ts
@@ -5,7 +5,7 @@ export = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.get('Authorization');
     if (!authHeader) {
         const error = new Error("Not authenticated");
-        res.send(401);
+        res.sendStatus(401);
         throw error
     }
     const token = authHeader.split(' ')[1];
@@ -18,7 +18,7 @@ export = (req: Request, res: Response, next: NextFunction) => {
     }
     if (!decodedToken) {
         const error = new Error('Not authenticated');
-        res.send(401);
+        res.sendStatus(401);
         throw error
     }
     req.userId = decodedToken.userId;
