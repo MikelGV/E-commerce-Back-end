a4908fe2cae169f90efb813b7f1cb0b5051e512e
MikelGV <mikelgaldosv@gmail.com>
2022-03-06 06:07:33 +0100

    login router created

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const CryptoJs = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/User");
 
 
@@ -18,6 +20,33 @@ router.post("/register", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("Wrong credential");
+
+        const hashedPassword = CryptoJs.AES.decrypt(user.password, process.env.PASSWORD_SECRET);
+        const OriginalPassword = hashedPassword.toString(CryptoJs.enc.Utf8);
+
+        const inputPassword = req.body.password;
+
+        OriginalPassword !== inputPassword && res.status(401).json("Wrong password");
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, process.env.JWT_SEC, {expiresIn:"3d"});
+
+        const {password, ...others} = user._doc;
+
+        res.status(200).json({...others, accessToken});
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
 
 
 module.exports = router;
