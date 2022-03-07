a96a20711145f6a522774d8a43e8dc12e1d2c6d7
MikelGV <mikelgaldosv@gmail.com>
2022-03-07 10:32:53 +0100

    get product function added

--- routes/product.js
+++ routes/product.js
@@ -35,7 +35,17 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
     } catch (err) {
         res.status(500).json(err)
     }
-})
+});
+
+// GET PRODUCT
+router.get("/find/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
 
 
 module.exports = router;
