a6b6188b575b6aedfb64f1f80e1b7496530aae03
MikelGV <mikelgaldosv@gmail.com>
2022-03-07 10:25:42 +0100

    delete product function added

--- routes/product.js
+++ routes/product.js
@@ -21,7 +21,17 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
         const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
             $set: req.body,
         }, {new: true});
-        res.status(200).json(updatedProduct)
+        res.status(200).json(updatedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// DELETE PRODUCT
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product Deleted");
     } catch (err) {
         res.status(500).json(err)
     }
