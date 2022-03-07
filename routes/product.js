2c95d63c7cb8ace42448a69021200675e0bd5401
MikelGV <mikelgaldosv@gmail.com>
2022-03-07 10:15:38 +0100

    create product added

---
+++ routes/product.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { verifyTokenAndAdmin } = require("./verifyToken");
+const Product = require("../models/Product");
+
+
+// CREATE PRODUCT
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product(req.body);
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+
+module.exports = router;
