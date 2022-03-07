a95222d433f608364a5b24dbbf483a265edc05a5
MikelGV <mikelgaldosv@gmail.com>
2022-03-07 11:16:56 +0100

    order route added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 
 const app = express();
@@ -27,6 +28,7 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(PORT || 500, () => {
     console.log("Backend is running");
